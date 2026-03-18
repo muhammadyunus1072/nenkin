d0c829f09be0b4125a8f174e0f27142f95b6706c
Muhammad Yunus <muhammadyunus1072@gmail.com>
2026-03-18 17:14:06 +0700

    update

--- database/seeders/User/UserSeeder.php
+++ database/seeders/User/UserSeeder.php
@@ -20,6 +20,21 @@ class UserSeeder extends Seeder
             'password' => Hash::make("123"),
         ]);
 
+        $user1 = User::create([
+            'name' => "Sabrina",
+            'email' => "sabrina@jexpertrecruitment.com",
+            'password' => '$2y$12$GFT63uHthafzJejvn8f0me/SKVQbHnIZENPzkoE0iGrSdeVQwInNe',
+            'phone' => "8118680136",
+        ]);
+        $user2 = User::create([
+            'name' => "Tanjung",
+            'email' => "tanjung@jexpertrecruitment.com",
+            'password' => '$2y$12$sGNTAnNvArw0bUfXIrXLPOMj128JlS0e6L0RbLmppOLmATHuHHm8e',
+            'phone' => "81220004752"
+        ]);
+
         $user->assignRole('Admin');
+        $user1->assignRole('Admin');
+        $user2->assignRole('Admin');
     }
 }
